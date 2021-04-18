4b7ba309e4a1807e3104f8779deaa570797b5ab9
sinkyoungdeok <tlsrudejr123@nate.com>
2021-04-18 17:06:35 +0900

    [ADD] 11053

---
+++ baekjoon/11000/11053.py
@@ -0,0 +1,17 @@
+"""
+문제 링크: https://www.acmicpc.net/problem/11053
+문제 접근 방법: dp
+"""
+
+N = int(input())
+
+
+A = list(map(int,input().split()))
+dp = [1] * N
+
+for i in range(N):
+    for j in range(i):
+        if A[i] > A[j]:
+            dp[i] = max(dp[i], dp[j] +1 )
+
+print(max(dp))
