65ad75ce6e1d784055b72bf561c4fb742b767956
singyeongdeog <tlsrudejr123@nate.com>
2021-04-18 23:53:16 +0900

    [ADD] 2003

---
+++ baekjoon/2000/2003.py
@@ -0,0 +1,26 @@
+"""
+문제 링크: https://www.acmicpc.net/problem/2003
+문제 접근 방법: dp
+"""
+
+N, M = map(int, input().split())
+
+A = list(map(int, input().split()))
+
+start = 0
+end = 1
+res = 0
+sum = A[start]
+if sum == M:
+    res += 1
+while not (start == end == N):
+    if sum < M and end < N:
+        sum += A[end]
+        end +=1
+    else:
+        sum -= A[start]
+        start +=1
+    if sum == M:
+        res += 1
+
+print(res)
