189ba66f92b0095160f654ad869d6db776d30370
sinkyoungdeok <tlsrudejr123@nate.com>
2021-04-19 09:47:11 +0900

    [ADD] 1806

---
+++ baekjoon/1800/1806.py
@@ -0,0 +1,35 @@
+"""
+문제 링크: https://www.acmicpc.net/problem/1806
+문제 접근 방법: dp
+"""
+
+N, S = map(int, input().split())
+
+num_list = list(map(int,input().split()))
+
+
+start = 0
+end = 1
+sum = num_list[start]
+res = N
+check = False
+
+if sum >= S:
+    res = 1
+
+while not ( N == start == end):
+    if sum < S and end < N:
+        sum += num_list[end]
+        end += 1
+    else:
+        sum -= num_list[start]
+        start +=1
+    
+    if sum >= S:
+        res = min(res,end-start)
+        check = True
+    
+if check:
+    print(res)
+else:
+    print(0)
