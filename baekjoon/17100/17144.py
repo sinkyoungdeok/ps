21484416c5303075ed8ecc22496ea10c44e43b4c
sinkyoungdeok <tlsrudejr123@nate.com>
2021-04-22 13:18:37 +0900

    [ADD] 17144

--- baekjoon/17100/17144.py
+++ baekjoon/17100/17144.py
@@ -3,64 +3,67 @@
 문제 접근 방법: 구현 
 """
 
-R, C, T = map(int,input().split())
-
-B_main = [ list(map(int,input().split())) for _ in range(R)]
-B_temp = [ [0] * C for _ in range(R) ]
+from copy import deepcopy
 
 di = [1,-1,0, 0]
 dj = [0,0, 1,-1]
+R, C, T = map(int,input().split())
+B = []
+cleaner = []
 
-clean_up_i = -1
-clean_up_j = -1
-clean_down_i = -1
-clean_down_j = -1
-# 공기 청정기의 윗부분 위치 찾기 
+def diffusion():
+    temp = [[0] * C for _ in range(R)]
+    temp[cui][cuj] = -1
+    temp[cdi][cdj] = -1
+    for i in range(R):
+        for j in range(C):
+            if B[i][j] != -1:
+                cnt = 0
+                for d in range(4):
+                    ni = i + di[d]
+                    nj = j + dj[d]
+                    if 0<= ni < R and 0 <= nj < C and B[ni][nj] != -1:
+                        temp[ni][nj] += B[i][j] // 5
+                        cnt += 1
+                temp[i][j] += B[i][j] - (B[i][j] // 5 * cnt)
+    return temp
+
+def clean(i, j, direction):
+    temp = deepcopy(B)
+    ci, cj = i, j+1
+    B[ci][cj] = 0
+    for d in range(4):
+        while True:
+            ni = ci + di[direction[d]]
+            nj = cj + dj[direction[d]]
+
+            if ni == i and nj == j:
+                return  
+            
+            if 0 <= ni < R and 0 <= nj < C:
+                B[ni][nj] = temp[ci][cj]
+            else:
+                break 
+            ci, cj = ni, nj 
+
 for i in range(R):
+    temp = list(map(int,input().split()))
+    B.append(temp)
     for j in range(C):
-        if B_main[i][j] == -1:
-            clean_up_i = i-1
-            clean_up_j = j
-            clean_down_i = i
-            clean_down_j = j
+        if B[i][j] == -1:
+            cleaner.append((i,j))
 
+cui, cuj = cleaner[0]
+cdi, cdj = cleaner[1]
 
-# 미세먼지 확산 (일단 확산되는 수치들은 B_temp에 저장)
+for t in range(T):
+    B = diffusion()
+    clean(cui, cuj, [2, 1, 3, 0])
+    clean(cdi, cdj, [2, 0, 3, 1])
+
+B[cui][cuj] = 0
+B[cdi][cdj] = 0
+res = 0
 for i in range(R):
-    for j in range(C):
-        cnt = 0
-        for d in range(4):
-            ni = i + di[d]
-            nj = j + dj[d]
-
-            if 0 <= ni < R and 0<= nj < C and B_main[ni][nj] != -1 and B_main[i][j] != -1:
-                cnt +=1
-                B_temp[ni][nj] += B_main[i][j] // 5
-        B_temp[i][j] -= (B_main[i][j] // 5) * cnt 
-
-
-# 미세먼지 확산 반영
-for i in range(R):
-    for j in range(C):
-        B_main[i][j] += B_temp[i][j]
-        B_temp[i][j] = 0 
-
-# 공기 청정기 작동 (위) .. B_temp를 활용해야 할듯
-ci = clean_up_i
-cj = clean_up_j + 1
-temp_cur = B_main[ci][cj]
-temp_next = B_main[ci][cj+1]
-while cj + 1 < C:
-    ni = ci
-    nj = cj + 1
-    
-    temp = B_main[ci][cj]
-    B_main[ci][cj] = temp_next
-    temp_cur = B_main[ni][nj]
-    B_main[ni][nj] = temp_next
-    temp_next = 
-
-    cj = nj
-
-for i in B_main:
-    print(i)
+    res += sum(B[i])
+print(res)
