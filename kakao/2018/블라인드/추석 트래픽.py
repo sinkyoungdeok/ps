fb9cbbbdb4525d716c3188600210d5173e7cd576
sinkyoungdeok <tlsrudejr123@nate.com>
2021-05-01 15:55:15 +0900

    [ADD] 추석 트래픽

---
+++ kakao/2018/블라인드/추석 트래픽.py
@@ -0,0 +1,33 @@
+"""
+문제 링크: https://programmers.co.kr/learn/courses/30/lessons/17676
+문제 접근 방법: 구현
+"""
+
+def solution(lines):
+    answer = 0
+    log_list = []
+    for line in lines:
+        date, S, T = map(str, line.split())
+
+        hh, mm, ss = map(str, S.split(":"))
+        end_time = int(ss.split(".")[1]) + int(ss.split(".")[0]) * 1000 + int(mm) * 1000 * 60 + int(hh) * 1000 * 3600
+        T_time = int(float(T[:-1]) * 1000)
+        start_time = end_time - T_time + 1
+
+        log_list.append((start_time, end_time))
+
+    for ist, iet in log_list:
+        start1, end1 = ist, ist + 999
+        start2, end2 = iet, iet + 999
+        cnt1, cnt2 = 0, 0
+        for jst, jet in log_list:
+            if start1 <= jet  and jst <= end1:
+                cnt1+=1
+            if start2 <= jet  and jst <= end2:
+                cnt2+=1
+        answer = max(answer, cnt1)
+        answer = max(answer, cnt2)
+
+
+
+    return answer
