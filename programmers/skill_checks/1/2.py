b024ba6d9f4ee7deec29ca632e9ae5b35be95e3b
sinkyoungdeok <tlsrudejr123@nate.com>
2021-05-10 12:17:36 +0900

    [ADD] level 1 complete'

---
+++ programmers/skill_checks/1/2.py
@@ -0,0 +1,11 @@
+def solution(n):
+    answer = ''
+    for i in range(n):
+        if i%2 == 0:
+            answer+= "수"
+        else:
+            answer+= "박"
+    return answer
+
+n = 3
+print(solution(n))
