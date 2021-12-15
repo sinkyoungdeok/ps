104ff83559faed4b43f2a3350d94094256bc8813
sinkyoungdeok <tlsrudejr123@nate.com>
2021-12-15 22:25:10 +0900

    [ADD] (baekjoon) 1000.kt

---
+++ baekjoon/1000/1000.kt
@@ -0,0 +1,9 @@
+import java.util.Scanner
+fun main(){
+    val scanner:Scanner = Scanner(System.`in`)
+    var a = scanner.nextInt()
+    var b = scanner.nextInt()
+    
+    var c = a + b
+    println(c);
+}
