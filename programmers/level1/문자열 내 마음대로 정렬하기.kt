e0c3c325b3a59d017be47c6dfb1ec8c77a81cd57
sinkyoungdeok <tlsrudejr123@nate.com>
2021-12-17 23:59:03 +0900

    [ADD] (programmers) 문자열 내 마음대로 정렬하기..kt

---
+++ programmers/level1/문자열 내 마음대로 정렬하기.kt
@@ -0,0 +1,8 @@
+class Solution {
+    fun solution(strings: Array<String>, n: Int): Array<String> {
+        return strings.also {
+            it.sort()
+            it.sortBy { it[n] }
+        }
+    }
+}
