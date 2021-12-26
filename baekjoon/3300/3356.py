5cb894ad3468d3385d5227bdddffbb5407bd95f7
sinkyoungdeok <tlsrudejr123@nate.com>
2021-12-26 12:55:59 +0900

    [ADD] (baekjoon) 3356.py

---
+++ baekjoon/3300/3356.py
@@ -0,0 +1,21 @@
+
+def pi(pattern):
+    pi = [0] * len(pattern)
+    j = 0
+    for i in range(1, len(pattern)):
+        while j > 0 and pattern[i] != pattern[j]:
+            j = pi[j-1]
+
+        if pattern[i] == pattern[j]:
+            j += 1
+            pi[i] = j
+    return pi
+
+
+L = int(input())
+S = input()
+
+pi_list = pi(S)
+print(L-pi_list[-1])
+
+
