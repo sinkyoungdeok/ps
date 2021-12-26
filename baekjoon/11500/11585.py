ce10bff25fd2af79685299a738e08ace98b9daf3
sinkyoungdeok <tlsrudejr123@nate.com>
2021-12-26 11:38:30 +0900

    [ADD] (baekjoon) 11585.py

---
+++ baekjoon/11500/11585.py
@@ -0,0 +1,41 @@
+
+def pi(pattern):
+    pi = [0] * len(pattern)
+    j = 0
+    for i in range(1, len(pattern)):
+        while j > 0 and pattern[i] != pattern[j]:
+            j = pi[j-1]
+
+        if pattern[i] == pattern[j]:
+            j += 1
+            pi[i] = j
+    return pi
+
+def kmp(s, pattern):
+    pi_list = pi(pattern)
+    res = 0
+    j = 0
+    for i in range(len(s)):
+        while j > 0 and s[i] != pattern[j]:
+            j = pi_list[j-1]
+
+        
+        if s[i] == pattern[j]:
+            if j == (len(pattern)-1):
+                res += 1
+                j = pi_list[j]
+            else:
+                j+=1
+    return res
+
+N = int(input())
+target = list(input().split())
+now = list(input().split())
+now += now[:-1]
+
+res = kmp(now, target)
+
+for i in range(res, 0, -1):
+    if res % i ==0 and N % i == 0:
+        print("{0}/{1}".format(res//i, N//i))
+        break
