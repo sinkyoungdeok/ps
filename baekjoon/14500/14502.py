ba4c8a46323859050c66e8b0b65400f175fedc80
kyoungdeok.sin <kyoungdeok.sin@meshkorea.net>
2022-02-05 20:31:49 +0900

    [ADD] (baekjoon) 14502.py

--- baekjoon/14500/14502.py
+++ baekjoon/14500/14502.py
@@ -1,50 +1,66 @@
-from collections import deque
 from itertools import combinations
 from copy import deepcopy
 
-N, M = map(int,input().split())
-map = [ list(map(int,input().split())) for _ in range(N)]
+N, M = map(int, input().split())
 
+board = []
 wall = []
-virus = []
-blank_cnt = 0
-res = 0
-
-def bfs(virus):
-    di = [0, 0, 1, -1]
-    dj = [1, -1, 0, 0]
-    q = deque(virus)
-    cnt = 0
-
-    while q:
-        ci, cj = q.popleft()
-        for d in range(4):
-            ni, nj = ci + di[d], cj + dj[d]
-            if not (0 <= ni < N and 0 <= nj < M):
-                continue
-            if temp_map[ni][nj] != 0:
-                continue
-            temp_map[ni][nj] = 2
-            cnt += 1
-            q.append((ni,nj))
-
-    return cnt
 
 for i in range(N):
+    board.append(list(map(int, input().split())))
     for j in range(M):
-        if map[i][j] == 0:
+        if board[i][j] == 0:
             wall.append((i,j))
-            blank_cnt += 1
-        elif map[i][j] == 2:
-            virus.append((i,j))
 
-for w in combinations(wall, 3):
-    temp_map = deepcopy(map)
-    for i,j in w:
-        temp_map[i][j] = 1
+def diffusion(board):
+    for i in range(N):
+        for j in range(M):
+            if board[i][j] == 2:
+                dfs(board, i, j)
+    return
 
-    temp_res = blank_cnt - 3
-    temp_res -= bfs(virus)
-    res = max(res, temp_res)
+def dfs(board, ci, cj):
+    di = [0,0,1,-1]
+    dj = [1,-1,0,0]
+
+    for d in range(4):
+        ni, nj = ci+di[d], cj+dj[d]
+        if not(0 <= ni < N and 0 <= nj < M): continue
+        if board[ni][nj] != 0 : continue
+        board[ni][nj] = -1
+        dfs(board, ni, nj)
+
+
+
+
+    return
+
+def count(board):
+    cnt = 0
+    for i in range(N):
+        for j in range(M):
+            if board[i][j] == 0:
+                cnt +=1
+    return cnt
+
+res = 0
+
+for c in combinations(wall, 3):
+    # 벽 설치
+    for x,y in c:
+        board[x][y] = 1
+
+    temp = deepcopy(board)
+    diffusion(temp)
+    res = max(res, count(temp))
+
+    # 벽 초기화
+    for x,y in c:
+        board[x][y] = 0
 
 print(res)
+
+
+
+
+
