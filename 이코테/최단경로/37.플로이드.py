e657980ba9eeb0601c21f919c5088fcd7095168e
sinkyoungdeok <tkagmd1@naver.com>
2024-01-08 23:03:50 +0900

    [ADD] (이코테-최단경로) 플로이드

---
+++ 이코테/최단경로/37.플로이드.py
@@ -0,0 +1,24 @@
+n = int(input())
+m = int(input())
+
+INF = 200000000
+F = [[INF] * n for _ in range(n) ]
+
+for i in range(n):
+    F[i][i] = 0
+
+for i in range(m):
+    a,b,c = map(int,input().split())
+    if F[a-1][b-1] > c:
+        F[a-1][b-1] = c
+
+for k in range(n):
+    for i in range(n):
+        for j in range(n):
+            F[i][j] = min(F[i][j], F[i][k] + F[k][j])
+
+for i in range(n):
+    for j in range(n):
+        if F[i][j] == INF:
+            F[i][j] = 0
+    print(*F[i])
